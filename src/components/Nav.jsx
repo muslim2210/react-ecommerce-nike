49eae152e62f971364d5e303530ea949b0517636
muslim2210 <bukhori.muslim2210@gmail.com>
2024-01-24 11:29:29 +0700

    fix navbar on scroll change background

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,9 +6,25 @@ import { navLinks } from "../constants";
 
 const Nav = () => {
   const [toggleNavbar, setToggleNavbar] = useState(false);
+  const [navbar, setNavbar] = useState(false);
+
+  // change background navbar logic
+  const changeBackgorundNavbar = () => {
+    if (window.scrollY >= 80) {
+      setNavbar(true);
+    } else {
+      setNavbar(false);
+    }
+  };
+
+  window.addEventListener("scroll", changeBackgorundNavbar);
 
   return (
-    <header className="padding-x py-8 fixed z-50 w-full">
+    <header
+      className={`padding-x bg-white py-8 fixed z-50 w-full  ${
+        navbar ? "backdrop-blur-xl bg-white/70" : "bg-transparent"
+      }`}
+    >
       <nav className="flex justify-between items-center max-container">
         <a href="/">
           <img src={headerLogo} alt="Logo" width={130} height={29} />
@@ -37,10 +53,17 @@ const Nav = () => {
           />
         </div>
       </nav>
-      <div className={`mt-5 lg:hidden ${toggleNavbar ? "block" : "hidden"}`}>
-        <ul className="flex flex-col gap-4">
+      <div
+        className={`mt-5 backdrop-blur-md bg-white/30 lg:hidden ${
+          toggleNavbar ? "block" : "hidden"
+        }`}
+      >
+        <ul className="flex flex-col gap-4 ">
           {navLinks.map((item) => (
-            <li key={item.label} className="px-4 py-[10px] text-end mt-2">
+            <li
+              key={item.label}
+              className="px-4 py-[10px] text-center mt-2 hover:bg-black hover:bg-opacity-10 hover:text-white"
+            >
               <a href={item.href} className="font-montserrat">
                 {item.label}
               </a>
